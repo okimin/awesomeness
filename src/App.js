db23bdd687dfee0f1762b385e52081aa19179eea
Daniel Gaston <33844732+okimin@users.noreply.github.com>
2019-08-15 16:08:08 -0400

    React muy mal

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DO NOT LEARN REACT PLEASE.
         </a>
       </header>
     </div>
