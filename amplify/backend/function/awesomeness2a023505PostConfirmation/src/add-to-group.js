cc088dcdff9328e3d780c4b27ffea40a2849ab47
Daniel Gaston <danielgaston6@gmail.com>
2019-08-31 15:54:20 -0400

    Trying to start Auth

---
+++ amplify/backend/function/awesomeness2a023505PostConfirmation/src/add-to-group.js
@@ -0,0 +1,27 @@
+const aws = require("aws-sdk");
+
+exports.handler = (event, context, callback) => {
+  const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider(
+    { apiVersion: "2016-04-18" }
+  );
+
+  const email = event.request.userAttributes.email.split(".");
+  const domain = email[email.length - 1];
+
+  if (domain === "edu") {
+    const params = {
+      GroupName: "STUDENTS",
+      UserPoolId: event.userPoolId,
+      Username: event.userName
+    };
+
+    cognitoidentityserviceprovider.adminAddUserToGroup(params, err => {
+      if (err) {
+        callback(err);
+      }
+      callback(null, event);
+    });
+  } else {
+    callback(null, event);
+  }
+};
