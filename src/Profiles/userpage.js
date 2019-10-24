738108fc90a927880b1a5ce31d22ed1f742a14f3
Khristian Brooks <kh.brooks02@gmail.com>
2019-10-24 17:55:26 -0400

    style user profile

--- src/Profiles/userpage.js
+++ src/Profiles/userpage.js
@@ -1,56 +1,58 @@
-import React, { Component } from "react";
-import profilePic from "../images/construction.png";
-import { Container, Row, Col } from "react-bootstrap";
-import ProgressBar from "react-bootstrap/ProgressBar";
-import Image from "react-bootstrap/Image";
-import { Button } from "react-bootstrap";
-import "./userpage.css";
+import React, { Component } from 'react';
+import profilePic from '../images/10.jpg';
+import { Container, Row, Col } from 'react-bootstrap';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Image from 'react-bootstrap/Image';
+import { Button } from 'react-bootstrap';
+import './userpage.css';
 
 /* 
   The static user, we are going to make a 
   generic page that will accept any user.
+  NOTE: the profile pic must be a square image
 */
 const user = {
-  name: "Jessica Smith",
-  summary: "Young studious coder, graduate of MakeGamesWithUs",
-  img: profilePic,
-  rank: "QueenBee",
-  badges: [
-    { type: "stem", name: "Ecology" },
-    { type: "stem", name: "Chemistry" }
-  ]
+	name: 'Jessica Smith',
+	summary: 'Young studious coder, graduate of MakeGamesWithUs',
+	img: profilePic,
+	rank: 'QueenBee',
+	badges: [
+		{ type: 'stem', name: 'Ecology' },
+		{ type: 'stem', name: 'Chemistry' },
+	],
+	workshops: 32,
 };
+
 /*
   We can use a functional component since 
   we are not using React Lifecycle Methods (like componentDidMount)
   or state.
 */
 const userpage = () => {
-  return (
-    <div className="user">
-      <Container>
-        <Row className="profile">
-          <Col>
-            <Image src={user.img} height={150} roundedCircle />
-          </Col>
-          <Col className="name">
-            <p>{user.name}</p>
-            <p>{user.rank}</p>
-            <p>{user.summary}</p>
-          </Col>
-        </Row>
-        <Row className="logistics">
-          <Col>
-            <Row>
-              <Col></Col>
-            </Row>
-            # of Badges
-          </Col>
-          <Col># of Workshops</Col>
-        </Row>
-      </Container>
-    </div>
-  );
+	return (
+		<div className="user">
+			<Row className="profile">
+				<Col sm={4} className="profile-img">
+					<Image src={user.img} height={150} roundedCircle />
+				</Col>
+				<Col sm={8} className="name align-middle">
+					<p>{user.name}</p>
+					<p>{user.rank}</p>
+					<p>{user.summary}</p>
+				</Col>
+			</Row>
+			<Row className="logistics">
+				<Col>
+					<Row>{user.badges && user.badges.length}</Row>
+					<Row># of Badges</Row>
+				</Col>
+				<Col>
+					<Row>{user.workshops}</Row>
+					<Row># of Worshops</Row>
+				</Col>
+			</Row>
+		</div>
+	);
 };
 
 export default userpage;
