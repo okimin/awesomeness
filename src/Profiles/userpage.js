969ba10428505d389717906c9040bb3bd94cf70a
Khristian Brooks <kh.brooks02@gmail.com>
2019-10-28 19:21:07 -0400

    use firestore to get user data

--- src/Profiles/userpage.js
+++ src/Profiles/userpage.js
@@ -1,58 +1,74 @@
 import React, { Component } from 'react';
 import profilePic from '../images/10.jpg';
-import { Container, Row, Col } from 'react-bootstrap';
-import ProgressBar from 'react-bootstrap/ProgressBar';
+import { Row, Col, Spinner } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
-import { Button } from 'react-bootstrap';
 import './userpage.css';
+import firebase from '../firebase';
 
-/* 
-  The static user, we are going to make a 
-  generic page that will accept any user.
-  NOTE: the profile pic must be a square image
-*/
-const user = {
-	name: 'Jessica Smith',
-	summary: 'Young studious coder, graduate of MakeGamesWithUs',
-	img: profilePic,
-	rank: 'QueenBee',
-	badges: [
-		{ type: 'stem', name: 'Ecology' },
-		{ type: 'stem', name: 'Chemistry' },
-	],
-	workshops: 32,
-};
+var db = firebase.firestore();
 
-/*
-  We can use a functional component since 
-  we are not using React Lifecycle Methods (like componentDidMount)
-  or state.
-*/
-const userpage = () => {
-	return (
-		<div className="user">
-			<Row className="profile">
-				<Col sm={4} className="profile-img">
-					<Image src={user.img} height={150} roundedCircle />
-				</Col>
-				<Col sm={8} className="name align-middle">
-					<p>{user.name}</p>
-					<p>{user.rank}</p>
-					<p>{user.summary}</p>
-				</Col>
-			</Row>
-			<Row className="logistics">
-				<Col>
-					<Row>{user.badges && user.badges.length}</Row>
-					<Row># of Badges</Row>
-				</Col>
-				<Col>
-					<Row>{user.workshops}</Row>
-					<Row># of Worshops</Row>
-				</Col>
-			</Row>
-		</div>
-	);
-};
+// The following is to add users to our collection
+// const userRef = db.collection(“users”).add({
+//   fullname: this.state.fullname,
+//   email: this.state.email
+// });
 
-export default userpage;
+class UserPage extends Component {
+	constructor() {
+		super();
+		this.state = {
+			user: {},
+		};
+	}
+
+	componentDidMount() {
+		this.getUserAsync();
+	}
+
+	getUserAsync = async () => {
+		let usersCollection = db.collection('users').doc('qm8pw3LrLNYWPXm39Vhe');
+
+		let response = await usersCollection.get();
+		let user = await response.data();
+		console.log('I got the data you need: ', user);
+		this.setState({ user });
+	};
+
+	render() {
+		const { user } = this.state;
+		const userKey = Object.keys(user);
+		const isUserLoaded = userKey && userKey.length > 0;
+		return (
+			<div className="user">
+				<Row className="profile">
+					<Col sm={4} className="profile-img">
+						<Image src={profilePic} height={150} roundedCircle />
+					</Col>
+					<Col sm={8} className="name align-middle">
+						{isUserLoaded ? (
+							<div>
+								<p>{user.name}</p>
+								<p>{user.rank}</p>
+								<p>{user.summary}</p>
+							</div>
+						) : (
+							<Spinner animation="grow" />
+						)}
+					</Col>
+				</Row>
+				<Row className="logistics">
+					<Col>
+						<Row>{user.badges && user.badges.length}</Row>
+						<Row># of Badges</Row>
+					</Col>
+					<Col>
+						<Row>{user.workshops}</Row>
+						<Row># of Worshops</Row>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
+}
+
+export default UserPage;
